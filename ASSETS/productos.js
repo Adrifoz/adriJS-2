1758b6b2532b61746f0fb2a51ebbee4aed1b6129
adrifoz <adrianofadel7@gmail.com>
2024-09-06 23:36:18 -0300

    Img in cards

---
+++ ASSETS/productos.js
@@ -0,0 +1,9 @@
+ export const productos = [
+    {nombre: "Caño de agua termofusión", precio: 2000, cantidad: 1, imagen: '../ASSETS/IMG/CañoAgua.jpg'},
+    {nombre: "Caño de gas termofusión", precio: 4000, cantidad: 1, imagen: '../ASSETS/IMG/CañoGas.jpg'},
+    {nombre: "Caño epoxi", precio: 1500, cantidad: 1, imagen: '../ASSETS/IMG/CañoEpoxi.jpg'},
+    {nombre: "Caño corrugado", precio: 800, cantidad: 1, imagen: '../ASSETS/IMG/CañoCorrugado.jpg'},
+    {nombre: "Manguera", precio: 500, cantidad: 1, imagen: '../ASSETS/IMG/Manguera.jpg'},
+    ]
+
+ 
